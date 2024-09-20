7ac81d3d33f1387504c87021b21442bce10efda2
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2024-09-20 00:09:12 -0300

    feat: migrate, main, controller
    explain: criando a migração do banco, a main e o controller.

---
+++ BackEnd/migrate/migrate.go
@@ -0,0 +1,16 @@
+package migrate
+
+import (
+	"FullStackForm/configs"
+	"FullStackForm/models"
+)
+
+func init() {
+	configs.ConnectToDB()
+}
+
+func main() {
+	configs.DB.AutoMigrate(&models.Person{})
+}
+
+
